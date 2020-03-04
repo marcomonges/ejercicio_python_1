8d34284e986a4270f021d2d983c82f7e5cecd22f
Marco <monges.marco@gmail.com>
2020-03-04 17:14:40 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,16 +1,32 @@
-metros_cuadrados = int(input("Cuantos Metros Cuadrados tiene el estadio?: "))
-gente_gradas = int(input("Cantidad de gente que cabe en las gradas?: "))
-escenario = int(input("Qué porcentage de espacio ocupa el escenario?: "))
-precio_especial = int(input("Cuanto costará la entrada a precio especial?: ")) 
-precio_comun = int(input("Cuanto costará la entrada a precio común?: "))
+# CONSTANTES
+PERSONASENM2 = 4
+ESPACIOGRADAS = 0.2
+PRECIOESPECIAL = 0.3
+PRECIOCOMUN =  0.7
 
-boletos_total = (metros_cuadrados - escenario) * 4
-gente_total = (100 * gente_gradas) / 20
 
-total_precio_especial =
-total_precio_comun = 
-total_precio =
+# variables
 
-print("se estiman: " + gente_total + " personas en total")
-print("Tienes que imprimir: " + especial + " Boletos a precio especial, y: " + comun + " Boletos a precio común.")
-print("Las ganancias se estiman de: " + total_precio + " Pesos")
+totalM2 = float(input("Cantidad de metros cuadrados que tiene el estadio que contrató: "))
+perGrada = int(input("Capacidad de gente en la grada: "))
+porEscenario = int(input("el porcentaje de espacio ocupado por el escenario: "))
+
+# Cantidad total de Personas
+m2Grada = totalM2 * ESPACIOGRADAS
+m2Escenario = totalM2 * (porEscenario/100)
+m2disponibles = totalM2 - m2Grada - m2Escenario
+personas = (m2disponibles * 4) + perGrada
+
+print("Cantidad total de personas es: ", personas)
+
+
+# Ganancias Totales
+
+dineroEspecial = float(input("Ingrese el precio especial: "))
+dineroComun = float(input("Ingrese el precio común: "))
+
+precioEspecial = (personas * PRECIOESPECIAL) * dineroEspecial
+precioComun = (personas * PRECIOCOMUN) * dineroComun
+dineroTotal = precioEspecial + precioComun
+
+print("Si se venden todas las entradas, ingresaría", dineroTotal, "Pesos" )
